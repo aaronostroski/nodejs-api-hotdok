e9b64aad5b94b7f8bcc03a22cc6c3b278f0d2d53
aaronostroski <aaronostroski@gmail.com>
2019-04-09 02:16:15 -0300

    Componetizando arquivos de rotas e insert.

--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
 let express = require('express');
-let consign = require('consign');
+let combosRouter = require('./routes/combos');
 let app = express();
+
+app.use(combosRouter);
 
 const PORT = process.env.PORT || 3000;
 
-app.listen(PORT, ()=>{
+app.listen(PORT,  ()=>{
     console.info(`Servidor foi iniciado em ${PORT}`);
 })
--- src/routes/combos.js
+++ src/routes/combos.js
@@ -1,36 +1,32 @@
 let db = require('../db/connection')
 let CombosModel = require('../model/combos.model')
 let express = require('express');
-let app = express();
+let router = express.Router();
 
-const port = 3000;
-
-app.listen(3000, ()=>{
-    console.info(`Servidor HTTP escutando na porta ${port}`);
-})
-
-app.get('/combos', (req,res)=>{
-    CombosModel.find()
-        .then(doc => {
-            res.json(doc);
-            res.status(200);
-        })
-        .catch(err =>{
-            res.status(500).json(err)
-        })
-        
-})
-
-app.get('/combos/:id', (req,res)=>{
-    CombosModel.findOne({
-        _id: req.params.id
+    router.get('/combos', (req,res)=>{
+        CombosModel.find()
+            .then(doc => {
+                res.json(doc);
+                res.status(200);
+            })
+            .catch(err =>{
+                res.status(500).json(err)
+            })
+            
     })
-        .then(doc =>{
-            res.json(doc);
-            res.status(200);
-        })
-        .catch(err =>{
-            res.status(500).json(err)
-        })
 
-})
+    router.get('/combos/:id', (req,res)=>{
+        CombosModel.findOne({
+            _id: req.params.id
+        })
+            .then(doc =>{
+                res.json(doc);
+                res.status(200);
+            })
+            .catch(err =>{
+                res.status(500).json(err)
+            })
+
+    })
+
+module.exports = router;
